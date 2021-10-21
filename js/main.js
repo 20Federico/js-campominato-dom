f271a92b4e7f0a333a813c65513f81006e6baa79
20Federico <fefe.molino@gmail.com>
2021-10-21 17:54:19 +0200

    exercise completed

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const campoContainer = document.querySelector('.campo_container');
 const selectLevel = document.querySelector('nav select');
 // recupera l'elemento bottone
 const btnPlay = document.querySelector('nav button');
+////array che deve contenere tutti i numeri fino al nmax di caselle in base al livello
+let arrayCells;
 
 // eventListener al click del bottone che visalizza il valore della select
 btnPlay.addEventListener('click', 
@@ -15,10 +17,8 @@ function() {
 
   const cellsNumber = getCellNumber(level);
 
-  
   notBombCells(cellsNumber, 16);
   
-  let arrayCells;
   toCreateCells(cellsNumber);
 
 });
@@ -43,6 +43,24 @@ function getCellNumber(level) {
   return result
 };
 
+//funzione che determina un array contenente le celle che NON contengono bombe
+function notBombCells(cellsNumber, nBombCells, ) {
+  //ciclo che nserisce tutti i numeri nell'array
+  arrayCells = [];
+
+  for (let i = 1; i <= cellsNumber; i++) { //metti cellsNumber al posto del numero
+    arrayCells.push(i);
+  }
+  //ciclo ripetuto per n volte, in cui n sta per il numero di bombe che voglio
+  //per ogni bomba che voglio creare, genero un numero random per posizionarla casualmente
+  for (let i = 0; i < nBombCells; i++) { 
+    const nChoosenRandomly = randomNumber(1, cellsNumber)
+    arrayCells.splice(nChoosenRandomly - 1, 1);
+    cellsNumber--;
+  }
+  console.log(arrayCells);
+}
+
 //funzione che determina il ciclo per creare le celle
 function toCreateCells(cellsNumber)  {
 
@@ -58,74 +76,70 @@ function toCreateCells(cellsNumber)  {
     newCell.style.height = cellSize + '%';
     newCell.textContent = i + 1;
     newCell.setAttribute('index', i + 1);
+    newCell.setAttribute('clicked', 'false');
     newCell.addEventListener('click', newCellActive);
   }
 };
 
-//funzione che determina un array contenente le celle che NON contengono bombe
-function notBombCells(cellsNumber, nBombCells) {
-  //array che contiene tutti i numeri fno al nmax di caselle in base al livello
-  arrayCells = [];
-  //ciclo che nserisce tutti i numeri nell'array
-  for (let i = 1; i <= cellsNumber; i++) { //metti cellsNumber al posto del numero
-    arrayCells.push(i);
-  }
-  //ciclo ripetuto per n volte, in cui n sta per il numero di bombe che voglio
-  //per ogni bomba che voglio creare, genero un numero random per posizionarla casualmente
-  for (let i = 0; i < nBombCells; i++) { 
-    const nChoosenRandomly = randomNumber(1, cellsNumber)
-    arrayCells.splice(nChoosenRandomly - 1, 1);
-    cellsNumber--;
-  }
-  console.log(arrayCells);
-  return arrayCells;
-}
-
-
 let score = 0;
 //event listener al click di una cella per cambiare il colore di background 
 function newCellActive() {
-  
+
   const cellPosition = this.getAttribute('index');
   this.style.color = 'white';
 
-  //const nMaxCellsBlu = cellsNumber - nBombCells;
-  //console.log(nMaxCellsBlu);
+  if (this.getAttribute('clicked') === 'true') {
+    return;
+  }
   
   if (arrayCells.includes(parseInt(cellPosition))) {
+
     this.style.backgroundColor = '#6495ED';
     score++;
     console.log(score);
-    /*
-    if (score === cellsNumber - nBombCells) {
-      alert('complimenti! Hai vinto!!')
-    }
-    */
+    this.setAttribute('clicked', 'true');
 
   } else {
     this.style.backgroundColor = 'red';
-    setTimeout(endGame, 1);
+    setTimeout(gameOver, 1);
   }
+
+  setTimeout(gameCompleted, 1);
   
 }
 
-function endGame() {
+function gameCompleted() {
+  if (score === arrayCells.length) {
+    score = 0;
+    
+    const bomb = document.querySelectorAll('div.box[clicked=false]')
+    for (let i = 0; i < bomb.length; i++) {
+      const bombElement = bomb[i];
+      bombElement.style.backgroundColor = 'red';  
+    }
+    setTimeout(gameCompletedMsg, 100);  
+  }
+}
 
-  alert(`peccato, sei esploso. Hai ottenuto un punteggio di ${score}`);
+function gameCompletedMsg() {
+  
+  alert('complimenti! Hai vinto!!')
   campoContainer.innerHTML = '';
 
 }
 
+function gameOver() {
 
+  alert(`peccato, sei esploso. Hai ottenuto un punteggio di ${score} su ${arrayCells.length}`);
+  campoContainer.innerHTML = '';
+  score = 0;
 
-
+};
 
 //funzione che genera un numero random, dato un range
 function randomNumber(minNumber, maxNumber) {
+  
   const randomNum = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-  return randomNum
-}
+  return randomNum;
 
-
-//La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-//Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+};
